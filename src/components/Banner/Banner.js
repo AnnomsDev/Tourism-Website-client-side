0b46036f731307271d23510f2d30b29115154f23
AnnomsDev <foysalbinnour@gmail.com>
2021-10-29 17:30:40 +0600

    banner added

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,14 @@
+import './Banner.css'
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div className='banner'>
+            <h1>live your <br /> <span>adventure</span> </h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus non ad, illum unde sapiente</p>
+            <button className="btn-primary">Get Started</button>
+        </div>
+    );
+};
+
+export default Banner;
