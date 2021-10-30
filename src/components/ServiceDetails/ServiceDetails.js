f1c07772bd6e086f5c085f8029d1a66b4299c13d
AnnomsDev <foysalbinnour@gmail.com>
2021-10-30 19:14:35 +0600

    Place order btn functionality added

--- src/components/ServiceDetails/ServiceDetails.js
+++ src/components/ServiceDetails/ServiceDetails.js
@@ -1,21 +1,56 @@
 import './ServiceDetails.css'
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import axios from 'axios';
+import useFirebase from '../../Hooks/useFirebase';
 
 const ServiceDetails = () => {
     const { id } = useParams()
     const [service, setService] = useState({})
+    const { user } = useFirebase()
 
-    axios.get(`http://localhost:5000/services/${id}`)
-        .then(res => setService(res.data))
+    useEffect(() => {
+        axios.get(`http://localhost:5000/services/${id}`)
+            .then(res => setService(res.data))
+    }, [])
+
+    const handlePlaceOrder = () => {
+        const email = user.email;
+        const service_id = id;
+        const price = service.price;
+        const status = 'panding';
+        axios.post('http://localhost:5000/place-order', {
+            email,
+            service_id,
+            price,
+            status
+        })
+            .then(result => {
+                if (result.data.insertedId) {
+                    alert('Your order is placed')
+                }
+                else {
+                    alert("Something went wrong. please try again")
+                }
+            })
+            .catch(error => {
+                alert("Something went wrong. please try again")
+                console.log("got an error: ", error)
+            })
+
+    }
+
+
+
+
+
 
     return (
         <div>
             {/* <img src={service.img} alt="" /> */}
             <h2>{service.name}</h2>
             <p>{service.des}</p>
-            <button className='btn-secondery' >Book</button>
+            <button onClick={handlePlaceOrder} className='btn-secondery' >Place order</button>
         </div>
     );
 };
